2acf8e84dfefd2879aec50a495d62691d22ce807
James Schaffer <jas.schaffer@gmail.com>
2019-09-22 18:02:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,16 @@ def load_library(path)
   emoticon_library = YAML.load_file(path)
   
   
-  new_hash
+  new_hash = {
+    :get_meaning => emoticon_library.reduce({}) { |m_hash, (m_key, m_value)|
+      
+    } 
+    :get_emoticon => emoticon_library.reduce({}){ |e_hash, (e_key, e_value)|
+      
+    }
+      
+    }
+  }
 end
 
 def get_japanese_emoticon
