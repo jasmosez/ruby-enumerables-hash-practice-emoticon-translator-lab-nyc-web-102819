e6c7a3292c88de6006b7269bb2a2bc1b0bb767d8
James Schaffer <jas.schaffer@gmail.com>
2019-09-22 18:39:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(path)
   
   new_hash = {
     :get_meaning => emoticon_library.reduce({}) { |meaning_hash, (meaning_key, ej_array)|
-      meaning_hash[ej_array[1]] = meaning_key
+#      meaning_hash[ej_array[1]] = meaning_key
     }, 
     :get_emoticon => emoticon_library.reduce({}){ |e_hash, (e_key, e_value)|
     e_hash
